9fa2d59051758425888320c8e430fcfa0315e5e2
Jakub Hoczek <hoczek.jakub@gmail.com>
2021-05-22 08:38:53 +0200

    Update 22.05.2021 rano

--- Components/button.py
+++ Components/button.py
@@ -1,21 +1,21 @@
 import pygame
-
+# ZMIANY SELF IMG i top left
 class Button():
     def __init__(self,x,y,image,scale):
         width = image.get_width()
         height = image.get_height()
-        self.img = pygame.transform.scale(image,int(width) * scale,int(height) * scale)
-        self.imgRect = self.img.get_rect()
-        self.imgRect.topleft(x,y)
+        self.img = pygame.transform.scale(image, (int(width * scale),int(height * scale)))
+        self.rect = self.img.get_rect()
+        self.rect.topleft = (x,y)
         self.Clicked = False
     def Draw(self, surface):
         action = False
         pos = pygame.mouse.get_pos()
-        if (self.imgRect.collidepoint(pos)):
+        if (self.rect.collidepoint(pos)):
             if (pygame.mouse.get_pressed()[0] == 1 and self.Clicked == False):
                 action = True
                 self.Clicked = True
         if (pygame.mouse.get_pressed()[0] == 0):
             self.Clicked = False
-        surface.blit(self.img,(self.imgRect.x,self.imgRect.y))
+        surface.blit(self.img,(self.rect.x,self.rect.y))
         return action
--- State/EditorState/editor.py
+++ State/EditorState/editor.py
@@ -1,6 +1,9 @@
 from State.state import State
+# Nowe podpiecia:
+from Components.button import Button
 import pygame
-# Nowe elementy: tlo, animacja tla, przewijanie, tick Clocka, Nowe rzeczy konstruktor
+import csv
+# Nowe elementy:
 class Editor(State):
     def __init__(self,game):
         super().__init__(game)
@@ -25,8 +28,23 @@ class Editor(State):
         self.pineForestImg2 = pygame.image.load('./Sprites/Background/pine1.png').convert_alpha()
         #self.pineRect2 = self.pineForestImg2.get_rect()
         self.TILE_COUNT = 16
-        self.InitTiles()
-    def InitTiles(self):
+        # Nowe elementy:
+        self.level = 0
+        self.saveButtonImg = ''
+        self.loadButtonImg = ''
+        self.worldData = []
+        self.buttonList = []
+        self.currentTile = 0
+        self.init = True # Przerobic na kontrolke.
+        self.mousePos = 0
+        self.mousePosX = 0
+        self.mousePosY = 0
+        # INIT
+        #self.InitTiles() # TODO: Przeniesc to
+        #self.InitWorldData()
+    def InitTiles(self): # ZMIENIC NAZWE NA INIT IMG
+        self.saveButtonImg = pygame.image.load('img/saveButton.png').convert_alpha()
+        self.loadButtonImg = pygame.image.load('img/loadButton.png').convert_alpha()
         self.imgList = []
         for x in range(5):
             img = pygame.image.load(f'Sprites/Tiles/grass_{x+1}.png')
@@ -37,6 +55,45 @@ class Editor(State):
             img = pygame.transform.scale(img, (self.TILESIZE, self.TILESIZE))
             self.imgList.append(img)
         print(self.imgList)
+    # NOWE FUNKCJA:
+    def InitWorldData(self):
+        for row in range(self.ROWS):
+            r = [-1] * self.COLS
+            self.worldData.append(r)
+        print(self.worldData)
+    def InitButtons(self):
+        buttonCol = 0
+        buttonRow = 0
+        for i in range(len(self.imgList)):
+            tileButton = Button(self.game.DISPLAY_WIDTH - self.SIDEMARGIN + (75 * buttonCol) + 50, 75 * buttonRow + 50, self.imgList[i],1)
+
+            self.buttonList.append(tileButton)
+            buttonCol += 1
+            if buttonCol == 2:
+                buttonRow += 1
+                buttonCol = 0
+        self.saveButton = Button(0.02 * self.game.DISPLAY_WIDTH, self.game.DISPLAY_HEIGHT - self.LOWERMARGIN + 40, self.saveButtonImg, 0.35)
+        self.loadButton = Button(0.02 * self.game.DISPLAY_WIDTH + 75, self.game.DISPLAY_HEIGHT - self.LOWERMARGIN + 40, self.loadButtonImg, 0.35)
+    def InitEditorSettings(self):
+        self.InitTiles()
+        self.InitWorldData()
+        self.InitButtons()
+    def CheckMouseMotion(self):
+        self.mousePos = pygame.mouse.get_pos()
+        self.mousePosX = (self.mousePos[0] + self.scroll) // self.TILESIZE
+        self.MousePosY = self.mousePos[1] // self.TILESIZE
+    def CheckTileArea(self):
+        if self.mousePos[0] < self.game.DISPLAY_WIDTH - self.SIDEMARGIN and self.mousePos[1] < self.game.DISPLAY_HEIGHT - self.LOWERMARGIN:
+            # update tile value
+            if pygame.mouse.get_pressed()[0] == 1:
+                if self.worldData[self.mousePosY][self.MousePosY] != self.currentTile:
+                    self.worldData[self.MousePosY][self.mousePosX] = self.currentTile
+                    print("OK")
+                else:
+                    print(" NIE OK ")
+        print(self.mousePosX)
+        print(self.mousePosY)
+    # KONIEC NOWYCH
     def ResetKeys(self):
         self.keyBackspace = False
     def CheckInput(self):
@@ -66,6 +123,11 @@ class Editor(State):
                     self.scrollRight = True
                 if event.key == pygame.K_LSHIFT:
                     self.leftShift = True
+                # NOWE
+                if event.key == pygame.K_UP:
+                    self.level += 1
+                if event.key == pygame.K_DOWN and self.level > 0:
+                    self.level -= 1
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
                     self.scrollLeft = False
@@ -76,6 +138,26 @@ class Editor(State):
     def Update(self):
         self.UpdateEvents()
         self.CheckInput()
+        # NOWE
+        self.CheckMouseMotion()
+        self.CheckTileArea()
+        # TODO: ZROBIC NOWA FUNKCJE DO OBSLUGI CSV:
+
+        if self.saveButton.Draw(self.game.display):
+            # save level data
+            with open(f'level{self.level}_data.csv', 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=',')
+                for row in self.worldData:
+                    writer.writerow(row)
+        if self.loadButton.Draw(self.game.display):
+            # load in level data
+            # reset scrol lback to teh start of the level
+            self.scroll = 0
+            with open(f'level{self.level}_data.csv', newline='') as csvfile:
+                reader = csv.reader(csvfile, delimiter=',')
+                for x, row in enumerate(reader):
+                    for y, tile in enumerate(row):
+                        self.worldData[x][y] = int(tile)
         self.ResetKeys()
         pygame.display.update()
     def DrawBg(self):
@@ -88,24 +170,43 @@ class Editor(State):
             self.game.display.blit(self.mountainImage,((x * width)-self.scroll * 0.7, self.game.DISPLAY_HEIGHT - self.mountainImage.get_height() - 250))
             self.game.display.blit(self.pineForestImg2, ((x * width)-self.scroll * 0.8,self.game.DISPLAY_HEIGHT - self.pineForestImg2.get_height() - 180))
             self.game.display.blit(self.pineForestImg1, ((x * width)-self.scroll * 0.9,self.game.DISPLAY_HEIGHT - self.pineForestImg2.get_height() - 150))
-
     def DrawGrid(self):
         for i in range(self.COLS):
             pygame.draw.line(self.game.display,self.game.COLORS["white"],(i * self.TILESIZE - self.scroll,0),(i*self.TILESIZE - self.scroll,self.game.DISPLAY_HEIGHT- self.LOWERMARGIN))
         for i in range(self.ROWS):
             pygame.draw.line(self.game.display,self.game.COLORS["white"],(0,i * self.TILESIZE),(self.game.DISPLAY_WIDTH - self.SIDEMARGIN,i * self.TILESIZE))
-    def InitButtons(self):
-        pass
+    # NOWE
+    def DrawWorld(self):
+        for y, row in enumerate(self.worldData):
+            for x, tile in enumerate(row):
+                if tile >= 0:
+                    # screen.blit(imgList[tile],(x * TILESIZE - scroll,y * tile))
+                    self.game.display.blit(self.imgList[tile], (x * self.TILESIZE - self.scroll, y * self.TILESIZE))
+                    # screen.blit(imgList[tile],(x * TILESIZE - scroll,SCREEN_HEIGHT - TILESIZE))
     def DrawEditorPanel(self):
         pygame.draw.rect(self.game.display,self.game.COLORS['black'],(self.game.DISPLAY_WIDTH - self.SIDEMARGIN,0,self.SIDEMARGIN,self.game.DISPLAY_HEIGHT + self.LOWERMARGIN))
         pygame.draw.rect(self.game.display,self.game.COLORS["black"],(0,self.game.DISPLAY_HEIGHT - self.LOWERMARGIN,self.game.DISPLAY_WIDTH,self.game.DISPLAY_HEIGHT - self.LOWERMARGIN))
+        #buttonCount = 0
+        for buttonCount, i in enumerate(self.buttonList):
+            if i.Draw(self.game.display):
+                self.currentTile = buttonCount
+        # highlight the selected tile
+        pygame.draw.rect(self.game.display, self.game.COLORS['white'], self.buttonList[self.currentTile].rect, 3)
+        self.game.DrawText(f'Level - {self.level}', 20, 0.25 * self.game.DISPLAY_WIDTH,self.game.DISPLAY_HEIGHT - self.LOWERMARGIN // 2 + 5,self.game.COLORS["white"])
+        self.game.DrawText(f'Press UP or DOWN to change level', 20, 0.35 * self.game.DISPLAY_WIDTH + 100,self.game.DISPLAY_HEIGHT - self.LOWERMARGIN // 2 + 5,self.game.COLORS["white"])
+        self.saveButton.Draw(self.game.display)
+        self.loadButton.Draw(self.game.display)
+
     def Render(self):
         self.DrawBg()
         self.DrawGrid()
+        # NOWE
+        self.DrawWorld()
         self.DrawEditorPanel()
         self.BlitScreen()
     def DisplayState(self):
         print("Jestem w edytorze.")
+        self.InitEditorSettings()
         while self.runDisplay:
             self.Update()
             self.Render()
